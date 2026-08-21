ca46694255c5338069955ae0bbb259357a44bfa7
GT <galtamas777@gmail.com>
2026-08-21 13:24:43 +0300

    Meta Pixel Code updated in index.html

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -49,7 +49,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
         // 2. META / FACEBOOK PIXEL
-        (function(f,b,e,v,n,t,s) {
+        /*(function(f,b,e,v,n,t,s) {
             if(f.fbq)return;n=f.fbq=function(){n.callMethod?
             n.callMethod.apply(n,arguments):n.queue.push(arguments)};
             if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -64,6 +64,6 @@ document.addEventListener("DOMContentLoaded", function() {
         }(window, document,'script','https://connect.facebook.net/en_US/fbevents.js'));
         
         fbq('init', '891606847169072');
-        fbq('track', 'PageView');
+        fbq('track', 'PageView');*/
     }
 });
